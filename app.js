646697d5c69b691821ec94399993f0703317eef9
xSyhrx <rhyskouttswork@gmail.com>
2023-11-09 06:57:07 +1100

    Added Photo, github link'

--- app.js
+++ app.js
@@ -14,37 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
   menu.addEventListener('click', MobileMenu);
 
   // Highlight the active menu when scrolling
-  const highLightMenu = () => {
-    const element = document.querySelector('.highlight')
-    const homeMenu = document.querySelector('#home-page')
-    const aboutMenu = document.querySelector('#about-page')
-    const projectsMenu = document.querySelector('#projects-page')
-  
-    let scrollPos = window.scrollY
-  
-    // adds the 'highLights' class to my menu items
-    if(window.innerWidth > 960 && scrollPos < 600 ) {
-      homeMenu.classList.add('highlight')
-      aboutMenu.classList.remove('highlight')
-      return
-    } else if (window.innerWidth > 960 && scrollPos < 1400) {
-      aboutMenu.classList.add('highlight')
-      homeMenu.classList.remove('highlight')
-      projectsMenu.classList.remove('highlight')
-      return
-    }  else if (window.innerWidth > 960 && scrollPos < 2345) {
-      projectsMenu.classList.add('highlight')
-      aboutMenu.classList.remove('highlight')
-      return
-    }
-  
-    if((element && window.innerWidth < 960 && scrollPos < 600) || element) {
-      element.classList.remove('highlight')
-    }
-  }
-  window.addEventListener('scroll', highLightMenu)
-  window.addEventListener('click', highLightMenu)
-  
+ 
 
   // Close mobile menu when clicking on an item
   menuItems.forEach((item) => {
